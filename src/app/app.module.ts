9909f4486ece28c4de9d3981c62a4a44ed560fd7
Josef Shainskiy <josephs@arkia.co.il>
2019-06-02 15:47:13 +0300

    'ede'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { Test3Component } from './test3/test3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    Test3Component
   ],
   imports: [
     BrowserModule
